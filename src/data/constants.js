7375015d44babf33e1f7c5b9851456764d1c2a19
Deep Vyas <deepvyas2003@gmail.com>
2024-04-18 09:20:12 +0530

    resume added

--- src/data/constants.js
+++ src/data/constants.js
@@ -16,7 +16,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/DEEPVYAS03",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1PdsHzzSk-3D6pcGlcS6e7yihnVQAcLiJ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/deep-vyas-37a00122a/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
